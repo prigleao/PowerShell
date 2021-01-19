3d0b87832a01eab35d508f9cba8a6da249f010ba
Priscila Gusmão <prigleao@gmail.com>
2021-01-18 23:16:42 -0300

    Add files via upload

---
+++ Meu_TESTE.ps1
@@ -0,0 +1,25 @@
+﻿#teste com WScritp
+
+$wshell = New-object -com Wscript.shell
+$wshell | Get-Member
+$wshel.Popup("Teste no powershell")
+$wshell.run("Calc")
+
+#teste de execução de apps sem conector
+
+$Shell = new-Objetc -com WScript.shell
+$wshell.run("Notepad")
+$wshell.AppActivate("notepad")
+$wshell.SendKeys("Interessante!!!")
+
+#teste de variavel 1
+$variavel = "teste de variavel"
+Write-Output $variavel
+
+#Teste de variavel 2
+
+$variavelx = "Este e x"
+$variavely = "Este e y"
+[int]$numero = 20
+Write-Output "$variavelx,$variavely, $numero"
+
